2f78da996c49bb6566024889b18561f7c9b0b803
manish <manikmanshu@gmail.com>
2014-12-07 00:35:11 +0530

    Added instructions on how to play the game.

--- game.py
+++ game.py
@@ -2,7 +2,17 @@
 import os
 
 def main():
-    print "Welcome to guess the number\n==========================="
+    print "Welcome to guess the number game\n==========================="
+    print """
+    Instructions:
+    ------------
+    1) Guess a number between 0 to 100.
+    2) The game tells you how close you are to the guess.
+    3) It would also tell you whether you have guess higher
+       or lower than the previous guess.
+    4) Continue until you guess it right!
+    And you win... :-) Kudos!
+    """
 
     num = 42
     guess = ""
